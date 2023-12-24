6a2d7f13f27330d6a3333a44dde9ddca9071fbd6
sventro <polymonger@gmail.com>
2023-12-24 22:56:23 +1300

    working

--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -44,11 +44,12 @@ def find_gears(schematic):
         for part in schematic[1]:
             if row_adjacent(part, star):
                 if ranges_overlap(star, part):
-                    print(star, part)
+                    #print(star, part)
                     parts.append((int(part.value)))
-        print(parts)
+        #print(parts)
         if len(parts) == 2:
             gears.append(parts)
+    #print(gears)
     return gears
 
 
