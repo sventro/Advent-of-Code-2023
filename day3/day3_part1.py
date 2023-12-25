941e7d0405d304cf66401f78d4c5c7ec365f09da
sventro <polymonger@gmail.com>
2023-12-26 00:44:35 +1300

    updated day 3 part 1

--- day3/day3_part1.py
+++ day3/day3_part1.py
@@ -1,37 +1,56 @@
 import re
+from dataclasses import dataclass
+
+
+PARTS = r"(?!\.)(\d+|\D)"
+
+
+@dataclass(slots=True)
+class Part:
+    value: str
+    row: int
+    start: int
+    end: int
 
 
 def main():
     with open("input.txt") as file:
         lines = file.read().splitlines()
-    return sum(find_parts(lines))
+    return sum(find_parts(parse_schematic(lines)))
 
 
-def is_symbol(char):
-    return not char.isdigit() and char != "."
+def parse_schematic(schematic) -> tuple[list, list]:
+    symbols: list[Part] = []
+    parts: list[Part] = []
+    for row, line in enumerate(schematic):
+        for part in re.finditer(PARTS, line):
+            if not part.group().isdigit():
+                symbols.append(Part(part.group(), row, part.start(), part.end()))
+            else:
+                parts.append(Part(part.group(), row, part.start() - 1, part.end() + 1))
+    return parts, symbols
+
+
+def row_adjacent(part, symbol):
+    return (
+        part.row - 1 == symbol.row
+        or part.row == symbol.row
+        or part.row + 1 == symbol.row
+    )
+
+
+def ranges_overlap(part, symbol):
+    return range(max(symbol.start, part.start), min(symbol.end, part.end))
 
 
 def find_parts(schematic):
-    parts = []
-    width = len(schematic[0])
-    height = len(schematic)
-    for row, line in enumerate(schematic):
-        for number in re.finditer(r"\d+", line):
-            start = number.start() - 1
-            end = number.end()
-            part_number = int(number.group())
-            if start >= 0 and is_symbol(line[start]):
-                parts.append(part_number)
-            elif end < width and is_symbol(line[end]):
-                parts.append(part_number)
-            for i in range(start, end + 1):
-                if i >= len(line):
-                    continue  # to the next line
-                if row >= 0 and is_symbol(schematic[row - 1][i]):
-                    parts.append(part_number)
-                elif row < height - 1 and is_symbol(schematic[row + 1][i]):
-                    parts.append(part_number)
-    return parts
+    all_parts = []
+    for part in schematic[0]:
+        for symbol in schematic[1]:
+            if row_adjacent(part, symbol):
+                if ranges_overlap(part, symbol):
+                    all_parts.append((int(part.value)))
+    return all_parts
 
 
 if __name__ == "__main__":
